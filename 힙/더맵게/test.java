8db81dc2aa8d455f1f3e078307296c5e77d11414
wowDongHyeon <71442779+wowDongHyeon@users.noreply.github.com>
2020-10-06 23:17:10 +0900

    더 맵게 커밋
    -for(int i : array)  이걸 알게 되었다.
    -Collections.sort(arrayList) 라는 함수를 사용하면, arrayList가 정렬된다. 
    - 답은 맞췄지만 ,효율성은 0점이다. 그 이유는 잘모르겠다.

---
+++ 힙/더맵게/test.java
@@ -0,0 +1,55 @@
+import java.util.*;
+import java.util.Collections;
+
+public class MainTest {
+
+	public static void main(String[] args) {
+		int[] scoville= {1, 2, 3, 9, 10, 12};
+//		int[] priorities= {1, 1, 9, 1, 1, 1};4
+		int k=7;
+		System.out.println(Solution.solution(scoville, k)); 
+	}
+
+}
+
+class Solution {
+    public static int solution(int[] scoville, int K) {
+        int answer = 0;
+        ArrayList<Integer> scovilleList=getArrayListFromArray(scoville);
+        while(!checkOverK(scovilleList,K)) {
+        	if(scovilleList.size()>=2) {
+        		addScoville(scovilleList);
+        		answer++;
+        	}else {
+        		answer=-1;
+        		break;
+        	}
+        }
+        	
+        return answer;
+    }
+    private static ArrayList<Integer> getArrayListFromArray(int[] scoville) {
+		ArrayList<Integer> result=new ArrayList<Integer>();
+		for(int i: scoville) {
+			result.add(i);
+		}
+		return result;
+	}
+    private static boolean checkOverK(ArrayList<Integer> scovilleList, int K) {
+		for(int i : scovilleList) {
+			if(i<K) {
+				return false;
+			}
+		}
+		return true;
+	}
+    private static void addScoville(ArrayList<Integer> scovilleList) {
+    	Collections.sort(scovilleList);
+    	int sum=scovilleList.get(0)+scovilleList.get(1)*2;
+    	scovilleList.add(sum);
+    	scovilleList.remove(0);
+    	scovilleList.remove(0);
+	}
+	
+}
+
